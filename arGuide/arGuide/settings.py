760c7d429ef0fb962539498bc28e77e99e2bc9ba
Sylvia-laAhad <sylvia318136@gmail.com>
2024-06-04 14:23:23 +0800

    settings changes
    Allow host:localhost, 127.0.0.1
    addApp:main
    language:zh-hant
    Time zone: Asia/Taipei

--- arGuide/arGuide/settings.py
+++ arGuide/arGuide/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-68i9jyy)w4+&3%z0-_8p4u3^5jx99l_7v$k=6x-hcceiq7x-bw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hant'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
